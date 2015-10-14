72dbf34899cd1a034e285b585c8756636b06c10c
jccgadam <jccgadam@hotmail.com>
2015-10-14 09:48:49 -0700

    update user signup

--- client/templates/userAuth/userlogin.js
+++ client/templates/userAuth/userlogin.js
@@ -1,21 +1,30 @@
 
 Template.userlogin.events({
-    'blur input':function(e,t){
-          Session.set('message',null);
-    },
-	'click button':function(e,t){
+    
+    'submit .userlogin':function(e,t){
           e.preventDefault();
           var email = t.$('.email').val();
           var password =t.$('.password').val(); 
-          // if(email==null)
-          // {  
-          //    Session.set('message','email is empty');
-          // }
-          HTTP.post("http://52.89.233.213:9000/signin",
+          //check if email is empty
+          if(email=='')
+          {  
+             Session.set('emailError','Please enter email');
+             // console.log(Session.get('emailError'));
+             return;
+          }
+          //check if password is empty
+          else if(password=='')
+          { 
+            Session.set('passwordError','Please enter passord');
+            return;
+          }
+          else{
+
+            HTTP.post("http://52.89.233.213:9000/signin",
           {
-           data: {email:email, password:password}
+            data: {email:email, password:password}
           },
-          //server response
+          //server response callback
           function (error, response) {
             if (error) {
               console.log(error);
@@ -26,12 +35,26 @@ Template.userlogin.events({
               
             }
 
-	})
-}
+	          })
+               }
+    },
+    'blur .email':function(e,t){
+          Session.set('emailError',null);
+    },
+    'blur .password':function(e,t){
+          Session.set('passwordError',null);
+    },
+
 })
 
 Template.userlogin.helpers({
 	message:function(){
 	   return Session.get('message')
-	}
+	},
+  emailError:function(){
+    return Session.get('emailError')
+  },
+  passwordError:function(){
+    return Session.get('passwordError')
+  }
 })
---
+++ client/templates/userAuth/usersignup.js
@@ -0,0 +1,190 @@
+  function validateEmail(s){
+
+    var re = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;
+  	if(s=='')
+  		{
+  			Session.set('validateEmail','cannot be empty');
+  			return false;
+  		}
+    else if (s!=='')
+        {
+            if(!re.test(s))
+            {
+            	Session.set('validateEmail',' is not right formated');
+            	console.log(re.test(s));
+            	return false;
+            }
+            else{
+            	return true;
+            }
+        }
+  }
+
+  function validatePw(password){
+        if(password.length=='')
+        {
+        	Session.set('validatePw',' cannot be empty');
+        	
+        	return false;
+        }
+        else if(password.length<8)
+        {
+        	Session.set('validatePw',' at least 8 characters');
+        	return false;
+        }
+        else {
+
+        	return true;
+        	
+        }
+  }
+  function validateRePw(rpw,pw){
+  	    if(rpw=='')
+        {
+        	Session.set('validateRePw','re-password cannot be empty');
+        	return false;
+        }
+        else if(pw!=rpw)
+        {
+        	Session.set('validateRePw',' password does not match');
+        	return false
+        }
+        else{
+        	return true;
+        }
+  }
+  function validatefname(fname){
+  	     if(fname==='')
+        {
+        	Session.set('validatefname','cannot be empty');
+        	return false;
+        }
+        else{
+        	return true;
+        }
+  }
+  function validatelname(lname){
+  	    if(lname==='')
+        {
+        	Session.set('validatelname','cannot be empty');
+        	return false;
+        }
+        else{
+        	return true;
+        }
+  }
+
+  Template.usersignup.onCreated(function(){
+  	Session.set('validatePw',null);
+  	Session.set('validateEmail',null);
+  	Session.set('validatefname',null);
+  	Session.set('validatelname',null);
+  	// this.errors = new ReactiveArray();
+    
+  })
+
+  Template.usersignup.events({
+  	'blur .email':function(e,t){
+   
+	   Session.set('validateEmail',null);
+	   var inputEmail = e.target.value;
+	   // console.log(validateEmail(inputEmail));
+       return validateEmail(inputEmail);
+    },
+
+     'blur .password':function(e,t){
+       
+        Session.set('validatePw',null);
+        var password = e.target.value;
+        // console.log(validatePw(password));
+        return validatePw(password);
+        
+     },
+     'blur .re-password':function(e,t){
+
+     	Session.set('validateRePw',null);
+        var pw = t.$('.password').val();
+        var rpw = e.target.value;
+        // console.log(validateRePw(pw,rpw));
+	    return validateRePw(pw,rpw);
+     },
+     'blur .fname':function(e,t){
+        Session.set('validatefname',null);
+        var fname = e.target.value;
+        // console.log(validatefname(fname));
+        return validatefname(fname);
+     },
+     'blur .lname':function(e,t){
+        Session.set('validatelname',null)
+        var lname = e.target.value;
+        // console.log(validatelname(lname));
+        return validatelname(lname);
+        
+     },
+     'submit form':function(e,t){
+     	e.preventDefault();
+     	var email = t.$('.email').val();
+     	var pw    = t.$('.password').val();
+     	var rpw   = t.$('.re-password').val();
+     	var fname = t.$('.fname').val();
+     	var lname  = t.$('.lname').val();
+     	// console.log(validateEmail(email));
+     	// console.log(validatePw(pw));
+     	// console.log(validateRePw(pw,rpw));
+     	// console.log(validatefname(fname));
+     	// console.log(validatelname(lname));
+        if(!(validateEmail(email)&&validatePw(pw)&&validateRePw(pw,rpw)&&validatefname(fname)&&validatelname(lname)))
+           {          
+           	 
+           	 //console.log(!validateEmail(email)&&validatePw(pw)&&validateRePw(pw,rpw)&&validatefname(fname)&&validatelname(lname))
+             return false;
+           }
+     	HTTP.post("http://52.89.233.213:9000/signup",
+          {
+            data: {
+            	email:email,password:pw,firstName:fname,lastName:lname
+                  }
+          },
+          //server response callback
+          function (error, response) {
+            if (error) {
+              Session.set('message','email duplicates')
+                        }
+            else{
+              Session.set('message','response')
+              console.log(response);
+              Router.go('main')
+              
+            }
+          })
+  	 
+     }
+ })
+
+
+
+
+  Template.usersignup.helpers({
+  	validateEmail:function(){
+
+        return Session.get('validateEmail');
+  	    
+  	},
+  	validatePw:function(){
+
+  		return Session.get('validatePw');
+  	},
+  	validateRePw:function(){
+
+  		return Session.get('validateRePw');
+  	},
+  	validatefname:function(){
+
+  		return Session.get('validatefname');
+  	},
+  	validatelname:function(){
+
+  		return Session.get('validatelname');
+  	}
+  
+  })
