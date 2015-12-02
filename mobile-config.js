e64f6c31c5989d1fb680dd7cbb3038329c5f0139
feiyu <yinfeiyu43@gmail.com>
2015-12-02 14:48:58 -0800

    Add icons

--- mobile-config.js
+++ mobile-config.js
@@ -1,1 +1,15 @@
 App.accessRule("*");
+App.icons({
+  // iOS
+  'iphone': 'resources/icons/qanda_60x60.png',
+  'iphone_2x': 'resources/icons/qanda_120x120.png',
+  'iphone_3x': 'resources/icons/qanda_180x180.png',
+  'ipad': 'resources/icons/qanda_76x76.png',
+  'ipad_2x': 'resources/icons/qanda_152x152.png',
+
+  // Android
+  'android_ldpi': 'resources/icons/qanda_36x36.png',
+  'android_mdpi': 'resources/icons/qanda_48x48.png',
+  'android_hdpi': 'resources/icons/qanda_72x72.png',
+  'android_xhdpi': 'resources/icons/qanda_96x96.png'
+});
